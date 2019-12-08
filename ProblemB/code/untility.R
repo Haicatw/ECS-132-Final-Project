db83e2b7f6bdaefeff87c61966a644390774b5ca
Kirby Zhou <kirbyzhou@protonmail.com>
2019-12-07 19:10:08 -0800

    Add argument to train test split

--- ProblemB/code/untility.R
+++ ProblemB/code/untility.R
@@ -1,5 +1,9 @@
-train_test_split <- function(dataset, num_train) {
+train_test_split <- function(dataset, num_train, shuffle=TRUE, random_state=0) {
     total_rows <- nrow(dataset)
+
+    set.seed(random_state)
+    dataset <- dataset[sample(nrow(dataset)),]
+
     train_set <- dataset[1:num_train, ]
     test_set <- dataset[(1+num_train):total_rows, ]
     list(train_set, test_set)
