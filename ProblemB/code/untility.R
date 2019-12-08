dc1b74293c44aebab26b96211bb04f4d9814245e
Kirby Zhou <kirbyzhou@protonmail.com>
2019-12-08 12:34:28 -0800

    Add accuracy utility function

--- ProblemB/code/untility.R
+++ ProblemB/code/untility.R
@@ -8,3 +8,12 @@ train_test_split <- function(dataset, num_train, shuffle=TRUE, random_state=0) {
     test_set <- dataset[-indeces, ]
     list(train=train_set, test=test_set)
 }
+
+Mean_square_error <- function(y_true, y_pred) {
+    diff <- (y_true - y_pred)^2
+    return (mean(diff))
+}
+
+Accuracy <- function(y_true, y_pred) {
+    return (sum(ifelse(y_pred == y_true, 1, 0))/length(y_pred))
+}
