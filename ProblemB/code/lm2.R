2e554f663444e21dd42b42a8e76603201b95ef4f
RayCloud51 <RayCloud51@gmail.com>
2019-12-13 09:09:09 -0800

    check lm2

--- ProblemB/code/lm2.R
+++ ProblemB/code/lm2.R
@@ -5,19 +5,25 @@ data <- data.frame(day1[, c(9:13)])
 
 k = 3 # overall k value search
 x = 30 # predict day 30
-dl <- list()
-for (c in 1:5) {
-    for (i in 1:x) {
-        col <- c()
-        for (j in 1:k) {
-            col <- c(col, data[c][i+j,])
-        }
-        dl <- c(dl, list(t(col)))
-    }
-}
 
-l <- do.call(rbind.data.frame, dl)
-l <- t(l)
+get_data <- function(k,data) {
+    dl <- list()
+    d=k-1
+    for (i in 1:k) {
+        for (c in 1:5) {
+        
+            col <- c()
+            for (j in 0:d) {
+                col <- c(col, data[c][i+j,])
+            }
+            dl <- c(dl, list(t(col)))
+        }
+    }
+    l <- do.call(rbind.data.frame, dl)
+    l <- t(l)
+    return (l)
+}
+dl <- get_data(3,data)
 for (c in 1:10) {
     for(i in 1:x) {
         linearMod <- lm(l[,30] ~ l[,]
